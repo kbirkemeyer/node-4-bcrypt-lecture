d38d7fdcf34f1c62e909b6c7ec446b3aa235bedd
Katie <adlerk8@gmail.com>
2020-11-10 11:34:47 -0700

    donesies

--- server/authController.js
+++ server/authController.js
@@ -1,14 +1,57 @@
+// require bcrypt in your authController file
+const bcrypt = require('bcrypt');
+
 module.exports = {
-    register: (req, res) => {
-
+    register: async (req, res) => {
+        const db = req.app.get('db');
+        const { email, username, password } = req.body;
+        // This replaces .then and .catch and avoids callback hell
+        // Normally: db.check_user(email).res...etc
+        const user = await db.check_user(email);
+        if(user[0]) {
+            return res.status(409).send("User already exists")
+        }
+        // The 10 here means you add salt 10 times.
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
+        const [newUser] = await db.add_user([username, email, hash]);
+        req.session.user = {
+            userId: newUser.user_id,
+            email: newUser.email,
+            username: newUser.username
+        };
+        res.status(200).send(req.session.user);
     },
-    login: (req, res) => {
-
+    login: async (req, res) => {
+        const db = req.app.get('db');
+        const {email, password} = req.body;
+        const user = await db.check_user(email);
+        if (!user[0]){
+            res.status(401).send('Incorrect credentials')
+        } 
+        const authenticated = bcrypt.compareSync(password, user[0].password);
+        if(authenticated){
+            req.session.user = {
+                userId: user[0].user_id,
+                email: user[0].email,
+                username: user[0].username
+            }
+            res.status(200).send(req.session.user)
+        } else {
+            res.status(401).send('Incorrect credentials')
+        }
     },
     logout: (req, res) => {
-
+        // Removes information from the session and ends it
+        req.session.destroy();
+        res.sendStatus(200)
     },
+    // Is there a user object on the session? if so, grab it:
     getUser: (req, res) => {
-        
+        if(re.session.user){
+            res.status(200).send(req.session.user)
+        } else {
+            res.sendStatus(404)
+        }
     }
 }
--- server/index.js
+++ server/index.js
@@ -2,12 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const massive = require('massive');
 // require session
+const session = require('express-session');
 const auth = require('./authController');
-const {SERVER_PORT, CONNECTION_STRING} = process.env;
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env;
 
 const app = express();
 // Top level middleware
 app.use(express.json());
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 7}
+}))
 
 massive({
     connectionString: CONNECTION_STRING,
@@ -22,3 +29,8 @@ massive({
 // Enpoints
 
 app.listen(SERVER_PORT, () => console.log(`Connected to port ${SERVER_PORT}⛵⚓`))
+
+app.post('/auth/register', auth.register);
+app.post('/auth/login', auth.login);
+app.post('/auth/logout', auth.logout);
+app.get('/auth/get_user', auth.getUser);
