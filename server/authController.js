9bd20d274b86a71f0d873f919cace6a24f7ad4b0
Adam Kent <adamkent@Adams-MacBook-Pro.local>
2020-11-10 09:35:55 -0700

    set up controller

---
+++ server/authController.js
@@ -0,0 +1,14 @@
+module.exports = {
+    register: (req, res) => {
+
+    },
+    login: (req, res) => {
+
+    },
+    logout: (req, res) => {
+
+    },
+    getUser: (req, res) => {
+        
+    }
+}
